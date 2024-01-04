18d987fa8d9955089ed544c9e0023da95194cc38
jyoti <jyopund25@gmail.com>
2024-01-04 22:55:26 +0530

    strings

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Hey there')
+document.getElementById("demo").innerHTML="Hello " + "Dolly!";
+// console.log(test)
+
+let age=30;
+let age1=age;
+
+age=31;
+
+console.log(`age=${age} age1=${age1}`);
+let txt = "I can eat bananas all day";
+let x = txt.slice(9, 17);
+console.log(x)
